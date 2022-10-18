0447e719c199ca58e1bd1446c0ba594400585872
Adeoluwa Adeboye <adeneye97@yahoo.com>
2022-10-18 02:42:07 +0100

    :construction: Finished the Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import LandingPage from "./pages/LandingPage";
 import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
 	return (
@@ -12,7 +13,9 @@ function App() {
 				<HeaderComponent/>
 				<Routes>
 					<Route path="/" element={<LandingPage />} />
+					TODO: Start mocking the most searched page
 				</Routes>
+				<FooterComponent/>
 			</div>
 		</>
 	);
--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import StyledFooterComponent from '../styles/FooterComponent.styled'
 
 const FooterComponent = () => {
   return (
-    <div>FooterComponent</div>
+    <StyledFooterComponent>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <img src='img/CatwikiLogoWhite.svg' alt='Cat Wiki Logo'/>
+        </div>
+        <div className='col-sm-6 mt-2'>
+          <p>&copy; created by Adeoluwa Adeboye - devChallenge.io</p>
+        </div>
+      </div>
+    </StyledFooterComponent>
   )
 }
 
